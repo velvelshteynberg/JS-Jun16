c2583c747642558d187d72681a25d3fce3fb13f4
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-06-29 21:55:23 -0400

    images added and completed project

--- recordsAssignment/main.js
+++ recordsAssignment/main.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     console.log("greatness")
-
-    let button = document.querySelector('.submitButton');
-    button.addEventListener("click", (e) => {
+    let counter = 0;
+    let form = document.querySelector('.infoForm');
+    form.addEventListener("submit", (e) => {
         e.preventDefault();
+        console.log(e);
         
         let newDiv = document.createElement('div');
         newDiv.classList.add('recordDiv');
+        newDiv.id = "newDiv" + counter;
+        counter ++;
 
         let img1 = document.createElement("img");
-        img1.src = 'https://www.google.com/search?q=picture+of+vinyl+record&rlz=1C1CHBF_enUS873US873&sxsrf=ALeKk01XGsEgETlPMRXQOSURgqkdcPoMGA:1624978164807&tbm=isch&source=iu&ictx=1&fir=OVMv_kIWA7ZWDM%252Cd_7BYFWreTebAM%252C_&vet=1&usg=AI4_-kSLvrAjwCKOYVz06hn5gbZ5q_b7wg&sa=X&ved=2ahUKEwjBoJ-yi73xAhXDGs0KHZYBA1EQ9QF6BAgPEAE#imgrc=OVMv_kIWA7ZWDM';
+        img1.src = './images/vinyl.png';
         img1.classList.add('recordImage');
         let img1Div = document.createElement('div');
         img1Div.classList.add('img1Div');
@@ -17,14 +20,18 @@ document.addEventListener('DOMContentLoaded', (e) => {
         newDiv.appendChild(img1Div);
 
         let img2 = document.createElement("img");
-        img2.src = 'https://files.slack.com/files-pri/T01EGJP1MPA-F026960EESJ/love.png';
+        img2.src = './images/close.png';
         img2.classList.add('img2');
         let img2Div = document.createElement('div');
+        let input = document.createElement('input');
+        input.type = 'hidden';
+        input.value = newDiv.id;
         img2Div.classList.add('img2Div')
+        img2Div.appendChild(input);
         img2Div.appendChild(img2)
 
         let img3 = document.createElement("img");
-        img3.src = 'https://files.slack.com/files-pri/T01EGJP1MPA-F026CTAT9AR/close.png';
+        img3.src = './images/love.png';
         img3.classList.add('img3');
         img2Div.appendChild(img3);
         newDiv.appendChild(img2Div);
@@ -38,12 +45,14 @@ document.addEventListener('DOMContentLoaded', (e) => {
         miniDiv.appendChild(plusSign);
 
         let artist = document.createElement('h3');
-        artist.innerText = 'Ed Sheeran';
+        let artistic = document.querySelector('.artist').value;
+        artist.innerText = artistic;
         artist.classList.add('artist');
         miniDiv.appendChild(artist);
 
         let genre = document.createElement('h3');
-        genre.innerText = 'pop';
+        let theGenre = document.querySelector('#genre').value;
+        genre.innerText = theGenre;
         genre.classList.add('genre');
         miniDiv.appendChild(genre);
 
@@ -51,7 +60,19 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
        let parentDiv = document.querySelector('.rightSide');
        parentDiv.append(newDiv);
+
+
+       img2.addEventListener('click', (e) =>{
+                let parent = e.target.parentNode.firstChild.value;
+                let parentDiv = document.querySelector(`#${parent}`);
+                parentDiv.remove();
+            });
+
     });
+      
+
+   
+
    
 
 });
