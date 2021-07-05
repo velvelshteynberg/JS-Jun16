9c28fc1b6f4f4eccf75dd5ad831f4383f29f401b
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-07-05 10:35:02 -0400

    working on fetchAPI

--- Js-FetchApi-practice/app.js
+++ Js-FetchApi-practice/app.js
@@ -2,13 +2,35 @@ document.addEventListener(("DOMContentLoaded"), () => {
     console.log('greatness');
 
     let ul = document.querySelector('.ul');
-    console.log(ul);
 
     fetch("https://jsonplaceholder.typicode.com/users")
     .then((e) => e.json())
-    .then((apidata)=>{
-        console.log(apidata);
-    })
-    
+    .then((apidata) => {
+            apidata.forEach((element) => {
+                const button = document.createElement('button');
+                const div = document.createElement('div');
+                const li = document.createElement('li');
+                const leftSide = document.querySelector('.leftSide');
+                button.innerText = element.name;
+                button.classList.add('button');
+                div.appendChild(button);
+                li.appendChild(div);
+                ul.appendChild(li);
 
-});
+
+                button.addEventListener('click', (event) => {
+                        const rightSide = document.querySelector('.rightSide');
+                        rightSide.append(element);
+                });
+
+
+            });
+        })
+    .catch((e) => console.log('err', e));
+
+
+    
+        
+
+
+    });
