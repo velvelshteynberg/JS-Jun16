5bd7694790ec502ff7197e308304623e8dc953b5
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-07-12 20:32:54 -0400

    working on it

--- MPP-assignmenet/app.js
+++ MPP-assignmenet/app.js
@@ -3,68 +3,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
     const allParties = [];
+    let api = [];
+    let data = [];
     fetch('https://represent.opennorth.ca/representatives/house-of-commons/?offset=0&limit=336')
     .then((e) => e.json())
     .then((apidata) => {
         apidata.objects.forEach((element) => {
             allParties.push(element.party_name);     
         }) 
-        let data = new Set(allParties);
-        data.forEach((element) => {
-            let parentDiv = document.querySelector('.bottomDiv');
+        data = new Set(allParties);
+        api = apidata;
+        something(data, api);
+        })   
+        .catch((err) => console.log(err));
 
-            let childDiv = document.createElement('div');
-            let title = document.createElement('h2');
-            title.innerText = element; 
-            childDiv.appendChild(title);
+});
 
-            apidata.objects.forEach((object) => {
-                if (object.party_name === element){
-                    let mpp = document.createElement('h4');
-                    mpp.innerText = object.name;
-                    childDiv.appendChild(mpp);
+function something(data, apidata){
+    data.forEach((element) => {
+        let parentDiv = document.querySelector('.bottomDiv');
 
-                    mpp.addEventListener('click', () => {
-                        let top = document.querySelector('.topDiv');
-                        top.innerHTML = '';
-                        let rightDiv = document.createElement('div');
-                        rightDiv.classList.add('rightDiv');
-                        let leftDiv = document.createElement('div');
-                        leftDiv.classList.add('leftDiv');
+        let childDiv = document.createElement('div');
+        let title = document.createElement('h2');
+        title.innerText = element; 
+        childDiv.appendChild(title);
 
-                        let image = document.createElement('img');
-                        image.src = object.photo_url;
-                        image.classList.add('image');
+        apidata.objects.forEach((object) => {
+            if (object.party_name === element){
+                let mpp = document.createElement('h4');
+                mpp.innerText = object.name;
+                childDiv.appendChild(mpp);
 
-                        let button = document.createElement('button');
-                        button.innerText = 'Advice';
+                mpp.addEventListener('click', () => {
+                    let top = document.querySelector('.topDiv');
+                    top.innerHTML = '';
+                    let rightDiv = document.createElement('div');
+                    rightDiv.classList.add('rightDiv');
+                    let leftDiv = document.createElement('div');
+                    leftDiv.classList.add('leftDiv');
 
-                        rightDiv.appendChild(image);
-                        rightDiv.appendChild(button);
-                        top.appendChild(rightDiv);
+                    let image = document.createElement('img');
+                    image.src = object.photo_url;
+                    image.classList.add('image');
 
-                        button.addEventListener('click', () => {
-                            fetch('https://api.adviceslip.com/advice')
-                            .then((e) => e.json())
-                            .then((data) => {
-                                let advice = document.createElement('input');
-                                advice.value = data.slip.advice;
-                                advice.classList.add('advice');
-                                leftDiv.appendChild(advice);
-                                button.disabled = true;
-                            })
-                            .catch((err) => console.log(err))
+                    let button = document.createElement('button');
+                    button.innerText = 'Advice';
+
+                    rightDiv.appendChild(image);
+                    rightDiv.appendChild(button);
+                    top.appendChild(rightDiv);
+
+                    button.addEventListener('click', () => {
+                        let advice = document.createElement('input');
+                        advice.classList.add('advice');
+                        if (object.advice === undefined){
+                        fetch('https://api.adviceslip.com/advice')
+                        .then((e) => e.json())
+                        .then((data) => {
+                            advice.value = data.slip.advice;
+                            object.advice = advice.value;
                         })
-                       
-
-                        top.appendChild(leftDiv);
-                    })
-                parentDiv.appendChild(childDiv);
-                }
-        })
-
-        
-    })
-    .catch((err) => console.log(err))
-})
-})
+                        .catch((err) => console.log(err))
+                    } else {
+                        advice.value = object.advice;
+                    }
+                    leftDiv.appendChild(advice);
+                    button.disabled = true;
+                    });
+                    top.appendChild(leftDiv);
+                });
+            parentDiv.appendChild(childDiv);
+            }
+    });
+});
+}
